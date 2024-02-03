f4d0f52d3a2216fecdaf7ea60837057602e25c80
Benson Kamau <muirubensonkamau@gmail.com>
2024-02-03 11:31:21 +0300

    Added a Data Frame

--- App.py
+++ App.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+# Read a simple data frame using pandas
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [10, 8, 6, 4, 2]})
+st.write(df)
 
 st.title('Hello World')
 st.write('This is a simple hello world app')
