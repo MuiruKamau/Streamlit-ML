b12db4e22dfd4f37ff0bedbece2c820c8c237f7c
Benson Kamau <muirubensonkamau@gmail.com>
2024-02-03 11:32:18 +0300

    Deletions

--- App.py
+++ App.py
@@ -9,22 +9,4 @@ st.write(df)
 st.title('Hello World')
 st.write('This is a simple hello world app')
 
-import matplotlib.pyplot as plt
 
-# Your data
-x = [1, 2, 3, 4, 5]
-y = [10, 8, 6, 4, 2]
-
-# Create a figure and axis
-fig, ax = plt.subplots()
-
-# Plot the data
-ax.plot(x, y)
-
-# Customize the plot
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_title('Graph')
-
-# Show the plot
-st.pyplot(fig)
